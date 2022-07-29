16c6cefa8526365c668a4fcd386bcaef7796e7f9
ksbhsh03 <108450038+ksbhsh03@users.noreply.github.com>
2022-07-29 10:13:12 +0900

    213213

---
+++ day1/src/day1/day1.java
@@ -0,0 +1,140 @@
+package day1;
+import java.util.Scanner;
+public class day1 {
+
+		/*
+		2022-06-30
+		첫 자바 프로그램
+		By Austin
+		*/
+		// 전체 한번에 주석 컨틀롤 슬레시
+		
+		  public static void main(String[] args) {
+
+		   int x;
+		    int y;
+		    int z;
+
+		    x = 63;
+		      y = 18;
+		      z = 52;
+		    System.out.println("x값은 "+ x + "입니다" );
+		    System.out.println("y값은");  
+		    
+//		     int x;
+//		     int y;
+
+//		     x = 63;
+//		     y = 18;
+		    
+//		    System.out.println("x값은 "+ x + "입니다:");      
+//		     System.out.println("y값은 "+ y + "입니다");
+		// System.out.println("합계는 "+(x + y) +"입니다.")    
+		   
+
+
+
+
+		    
+		  // System.out.print("첫 java 프로그램입니다.");
+		  //  System.out.println("화면에 출력하고 있습니다."); 
+		    
+		  // System.out.println("첫 java 프로그램입니다.");
+		  // System.out.println("화면에 출력하고 있습니다.");  
+		  
+
+
+		    
+		// System.out.println("금");
+		// System.out.println("상");
+		// System.out.println("첨");
+		// System.out.println("화");
+		  // Scanner stdin = new Scanner(System.in);
+
+
+		  //   System.out.println("15 :");
+		  //   int x = stdin.nextInt();
+
+		  //   System.out.println("10을 더 한 값은" + (x + 10)+ "입니다");
+		  //   System.out.println("10을 뺀 값은" + (x - 10)+ "입니다");    
+
+
+
+		    
+		   // Scanner stdin = new Scaner (System.in);
+
+		 
+		    
+		    
+
+
+
+
+		    
+		   // Scanner scanner  = new Scanner (System.in); //스캐너 객체 생성 
+		   //                                             //57
+		    
+
+		    
+		   //  // System.out.println("57");
+		   //  // String input = scanner.nextLine();
+		   //  // int num = Integer.parseInt(input); //입력받은 문자열으 숫자로 변환
+
+		   //  // System.out.println("입력내용 :"+input);
+		   //  // System.out.printf("num=%d%n", num); 
+
+		    
+		    
+		    // System.out.println("Hello world!");  //"Hello world" 출력
+		    //  //system.out.prntln("코드의 실행을 중지시킬 경우도 사용");
+
+
+
+		    // int x= 10, y=20;
+		    //  int tmp = 0;
+		    // System.out.println("x:"+ x + "y:" + y);
+
+		    //    tmp = x; //tem =10
+		    //    x = y;   //x =20
+		    //    y = tmp; // y=10
+
+		    // // 파이선 x.y = y.x
+		    // System.out.println("x:"+ x + "y:" + y);
+
+		    // String name = "ja" + "va";  //ja라는 문자열과 va라는 문자열을 붙여서 문자열 변수 name에 할당한다.
+		    // String str = name + 8.0;   // name이라는 문자열 형 변수값과 8.0이라는 실수를 붙여서 문자열 변수 str의 할당
+
+		    // System.out.println(name);
+		    // System.out.println(str);
+		    // System.out.println(7 + " ");
+		    // System.out.println(" " + 7);
+		    // System.out.println(7 + "");
+		    // System.out.println("" + 7);
+		    // System.out.println("" + "");
+		    // System.out.println(7 + 7 + "");
+		    // System.out.println("" + 7 + 7); //앞에 문자가 있어서 자동 형변환이 된다.
+		     
+		    
+		    
+		      // System.out.println("82 + 17 = " + (82 + 17));
+		      // System.out.println("82 - 17 = " + (82 - 17));
+		    
+
+
+
+		    
+		    
+		    
+		    
+		    
+		    
+
+
+
+		  
+
+
+		    
+		  }
+		  
+		}
---
+++ day2/src/day2/day2.java
@@ -0,0 +1,305 @@
+package day2;
+//정수값 구하는법
+import java.util.Scanner;
+public class day2 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+	    System.out.println("삼각형 넓이를 구합니다");
+	  
+	  
+	  System.out.print("밑변 :");
+	double x = stdIn.nextDouble();
+	 System.out.print("높이 :");
+	double y = stdIn.nextDouble();
+	   System.out.println("넓이는"+ ((x * y)/2) +"입니다" );
+
+	  
+	  
+	     // public static void main(String[] args) {
+	//   Scanner stdIn = new Scanner(System.in);
+//	     System.out.println("x 값");
+	// double x = stdin.nexyDouble();
+//	     System.out.println("y 값");
+//	     doulbe y = stdin.netxtDouble();
+//	        System.out.println("합계는" + (x+y) + "입니다")
+
+
+	    
+	   // Scanner stdIn = new Scanner(System.in);
+	    
+	   // int stdIn = 1275;
+	    
+	     
+	   //  System.out.println("정수값");
+	   //  int x = stdIn.nextInt:  // 입력받은 정숫값을 x에 저장한다.
+	   //  System.out.println
+
+	    
+	    
+
+
+	    
+	    // System.out.println("Hello world!!!!!");
+
+
+
+	    
+	  }
+	}
+
+
+//	import java.util.*;
+//	class Main {
+//	public static void main(String[] args) {
+//
+//	 System.out.println("당신의 주민번호를 입력해주세요 : ");
+//	String num = stdIn.nextLine();
+//	char gender = num.charAt(7);
+//	switch (gender) {
+//	case '1' :
+//	System.out.print("당신은 2000년 이전에 출생한 남자입니다");
+//	break;
+//	case '2' :
+//	System.out.print("당신은 2000년 이전에 출생한 여자입니다");
+//	break;
+//	case '3' :
+//	System.out.print("당신은 2000년 이후에 출생한 남자입니다");
+//	break;
+//	case '4' :
+//	System.out.print("당신은 2000년 이전에 출생한 여자입니다");
+//	break;
+//	default:
+//	System.out.println("잘못입력하셨습니다");
+	//   System.out.print("당신의 점수를 입력하세요.(1~100)>");
+	// int score = scanner.nextInt();
+
+	// switch(score/10) {
+	//   case 9: case 10:
+//	     System.out.println("당신의 학점은 A입니다.");
+//	     break;
+	//   case 8:
+//	     System.out.println("당신의 학점은 B입니다.");
+//	     break;
+	//   case 7:
+//	     System.out.println("당신의 학점은 C입니다.");
+//	     break;
+	//   case 6:
+//	     System.out.println("당신의 학점은 F입니다.");
+//	     break;
+	// }
+	  
+
+	  
+	  
+	    
+
+	  
+	  
+	  // System.out.println("주민번호를 입력하세요");
+	  //  Scanner scanner = new Scanner(System.in);
+	  //   String unm = scanner.nextLine();
+
+	  //   char gender = unm.charAt(7);
+	  //   System.out.println(gender);
+
+	  //   switch (gender){
+	  //     case '1';  case'3';
+	  //      System.out.println("당신은 남자입니다.");
+	  //       break;
+	  //         case '2';  case '4';
+	  //       System.out.println("당신은 여자 입니다.");
+	      
+	        
+	    // }
+
+	  
+	    
+	  
+	  
+	  // int month = 0;
+	// Scanner scanner = new Scanner(System.in);
+	  
+	//   System.out.println("현재 월을 입력하세여");
+	//   month = scanner.nextInt();
+
+	//   switch (month){
+	    
+//	     case 1;
+//	     case 2;
+//	       System.out.println("현재는 겨울 입니다.");
+//	       break;
+//	     case 3: case 3: case 4:
+//	       System.out.Println("현재는 계절은 봄입니다")
+//	         case
+//	         }
+	   
+
+
+
+
+	  
+	//  int score = 0;
+	// String name;
+	// System.out.println("현재 월을 입력하세요 : ");
+	// score = stdIn.nextInt();
+	// if (score <= 2 || score >= 12 ) {
+	// name = "겨울";
+	// } else if (score >= 3 || <=6) {
+	// name = "봄";
+	// } else if (score >= 6 ||  ) {
+	// name = "여름";
+	// } else  {
+	// name = "가을";
+	// }
+	// System.out.printf("현재의 계절은 %s입니다.", name);
+	 } 
+	}
+	  // 
+	  // 학점 만들기 + 0 - 까지 만들기
+	// int score = 0;
+	//  char grade = ''; 
+	//   char opt = '';
+	//   //변수는 크게 지역변수 와 전역변수 {}이안의 지역이다
+	//  System.out.println("정수를 입력해 주세요");
+	//   Scanner scanner = new scanner(System.in);
+	//   score = scanner.nextInt();
+	//   System.out.println("당신의 점수는"+ score + "점입니다");
+	//   System.out.printf("당신의 점수는 %d 점입니다",score);
+
+	//  if (score >= 90){
+//	    grade = 'A';
+//	    if (score >= 98){
+//	      opt ='+';
+
+//	    }
+//	    else if  (score >= 94){
+//	      opt ='0';
+//	      else{
+//	       opt ='-';
+//	      }
+	    
+	//  }
+	//   else if (score >= 80){
+//	    grade = 'B';
+	//  }
+	//   else if (score >= 70){
+//	    grade = 'C';
+	//  }
+	// else{
+	//   grade = 'D';
+	// }
+	// System.out.printf("당신의 학점은 %d 입니다", score;
+
+	// }
+	// }
+	//   int score = 0;
+	//   char grade = ' ';
+
+	// System.out.print("숫자를 하나 입력하세요.>");
+	// Scanner scanner = new  Scanner(System.in);
+	//   score = scanner.nextInt();
+
+	//   if (score >= 90 ){
+//	     grade = 'A';
+	// }
+//	    else if(score >= 80  ){
+//	    grade = 'B';
+	// }
+	//   else if (score >= 70 ){
+	//   grade='C';
+	  
+	//  }
+	//   else{
+//	     grade = 'D';
+	//   }
+	// }
+
+	//  System.out.print("숫자를 하나 입력하세요.>");
+	// Scanner a = new Scanner(System.in);
+	// int input = a.nextInt();
+	// if(input %3 == 0){
+	// System.out.println("입력하신 숫자는 3의 배수 입니다.");
+	// } else {
+	// System.out.println("입력하신 숫자는 3의 배수가 아닙니다.");
+	// }
+
+	     
+	  
+	  
+	  
+	  //  int input;
+	//   System.out.print("숫자를 하나 입력하세요.>");
+	// Scanner scanner = new Scanner(System.in) ;
+	// String tmp = scanner.nextLine();
+	// input = Integer.parseInt(tmp);
+
+	// if(input==0) {
+	//   System.out.println("입력하신 숫자는 0입니다.");
+	// }
+	// if(input!=0) {
+	//   System.out.println("입력하신 숫자는 0이 아닙니다.");
+	//   System.out.printf("입력하신 숫자는 %d입니다.", input);
+	  
+	// }
+	  
+	  // if 구분에서 true 일 경우 구분은 중괄호을 하상 사용
+	 //  //정수형 변수 
+	 //  int x = 0;
+	 //  System.out.println("x=%d 일때, 참인 것은%n", x);
+	 //  //%d 자리표시 d정수 x에 초기밧 0을 대입
+	 //  if(x==0) System.out.println("x==0");
+	 //  if(x!=0) System.out.println("x!=0");
+	 //  if(!(x==0)) System.out.println("!(x==0)");
+	 //  if(!(!x=0)) System.out.println("!(!x=0)");
+	 //  x = 1;
+	 //    System.out.println("x=\d 일때, 참인 것은 %n" , x));
+	 //  if(x==0) System.out.println("x==0");
+	 //  if(x!=0) System.out.println("x!=0");
+	 //  if(!(x==0)) System.out.println("!(x==0)");
+	 //  if(!(!x=0)) System.out.println("!(!x=0)");
+	  
+
+	  
+	  
+	  // Scanner stdIn = new Scanner(System.in);
+//	     System.out.println("삼각형 넓이를 구합니다");
+	  
+	  
+	//   System.out.print("밑변 :");
+	// double x = stdIn.nextDouble();
+	//  System.out.print("높이 :");
+	// double y = stdIn.nextDouble();
+//	    System.out.println("넓이는"+ ((x * y)/2) +"입니다" );
+
+	  
+	  
+	     // public static void main(String[] args) {
+	//   Scanner stdIn = new Scanner(System.in);
+//	     System.out.println("x 값");
+	// double x = stdin.nexyDouble();
+//	     System.out.println("y 값");
+//	     doulbe y = stdin.netxtDouble();
+//	        System.out.println("합계는" + (x+y) + "입니다")
+
+
+	    
+	   // Scanner stdIn = new Scanner(System.in);
+	    
+	   // int stdIn = 1275;
+	    
+	     
+	   //  System.out.println("정수값");
+	   //  int x = stdIn.nextInt:  // 입력받은 정숫값을 x에 저장한다.
+	   //  System.out.println
+
+	    
+	    
+
+
+	    
+	    // System.out.println("Hello world!!!!!");
+
+
+
+	    
